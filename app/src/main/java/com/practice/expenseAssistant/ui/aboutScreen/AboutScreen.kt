307ad7301cda32573a719c249fd8a459156cb790
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2023-09-27 12:01:18 +0200

    about added

--- app/src/main/java/com/practice/expenseAssistant/ui/aboutScreen/AboutScreen.kt
+++ app/src/main/java/com/practice/expenseAssistant/ui/aboutScreen/AboutScreen.kt
@@ -2,10 +2,13 @@ package com.practice.expenseAssistant.ui.aboutScreen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.practice.expenseAssistant.ui.theme.ExpenseAssistantTheme
@@ -14,9 +17,35 @@ import com.practice.expenseAssistant.R
 @Composable
 fun AboutScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
+        Text(text = stringResource(R.string.my_name), style = MaterialTheme.typography.displayLarge)
+        Row(
+            modifier = Modifier.padding(top = dimensionResource(id = R.dimen.eight_dp)),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Mail,
+                contentDescription = stringResource(R.string.my_email_address),
+            )
+            Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.eight_dp)))
+            Text(
+                text = stringResource(R.string.faiqalikhan49_gmail_com),
+                style = MaterialTheme.typography.displaySmall,
+            )
+        }
         Text(
-            text = stringResource(id = R.string.about_me),
+            text = stringResource(id = R.string.about_me_header),
             style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.eight_dp))
+        )
+        Text(
+            text = stringResource(id = R.string.about_me_body),
+            style = MaterialTheme.typography.displaySmall,
+        )
+        // About Application
+        Text(
+            text = stringResource(id = R.string.about_application),
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.padding(vertical = dimensionResource(id = R.dimen.eight_dp))
         )
     }
 }
@@ -25,6 +54,10 @@ fun AboutScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun AboutScreenPreview() {
     ExpenseAssistantTheme {
-        AboutScreen(modifier = Modifier.fillMaxSize())
+        AboutScreen(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(all = dimensionResource(id = R.dimen.screen_content_padding))
+        )
     }
 }
