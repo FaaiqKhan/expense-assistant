b75e7783091fae30fce639e8b1fb96caf1f795cd
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2023-10-08 16:09:48 +0200

    theme and color updates

--- app/src/main/java/com/practice/expenseAssistant/ui/theme/Theme.kt
+++ app/src/main/java/com/practice/expenseAssistant/ui/theme/Theme.kt
@@ -79,30 +79,16 @@ private val darkColorScheme = darkColorScheme(
 @Composable
 fun ExpenseAssistantTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> darkColorScheme
-        else -> lightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+    val colors = if (!darkTheme) {
+        lightColorScheme
+    } else {
+        darkColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
